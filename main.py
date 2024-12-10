fcd3162e9d382d9a177daa17fa1f0939aab79788
Taifun2005 <lider2005@yandex.ru>
2024-12-10 20:19:58 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.dispatcher import FSMContext
 import asyncio
 
 api = "7890890424:AAEwL3AvXADeymJq1hsVzEH_kAkWYnGdBsc"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-@dp.message_handler(text=['Urban', 'ff'])
-async def urban_message(message):
-    print('Urban message')
+kb = InlineKeyboardButton()
+button1 = InlineKeyboardButton(text='Информация', callback_data='info')
 
-dp.message_handler(commands=['start'])
-async def start_message(mesage):
-    print("Start message")
+kb.add(button1)
 
-@dp.message_handler()
-async def all_message(message):
-    print("Мы получили сообщение!")
+# stat_menu = ReplyKeyboardMarkup(
+#     keyboard=[
+#         [KeyboardButton(text='info')],
+#         [
+#             KeyboardButton(text="shop"),
+#             KeyboardButton(text='donate')
+#         ]
+#     ], resize_keyboard=True
+# )
+
+
+@dp.message_handler(commands=['start'])
+async def starter(message):
+    await message.answer("Рады вас видеть", reply_markup=kb)
+
+
+@dp.callback_query_handler(text = 'info')
+async def infor(call):
+    await call.message.answer("Информация о боте")
+    await  call.answer()
 
 
 if __name__ == "__main__":
